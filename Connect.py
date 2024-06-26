6a7820fcfc15b7bb858df3c5f4dcd9a75f39da07
Danil Batrakov <72032848+cupiscube@users.noreply.github.com>
2024-06-26 10:15:34 +0300

    Update Connect.py

--- Connect.py
+++ Connect.py
@@ -10,8 +10,8 @@ class DBConnect:
         port = int(port)
     db_name = input('Введите название БД (если БД bregis_lis данный шаг можно пропустить нажав Enter): ')
     if db_name == '':
-        db_name = 'bregis_lis'
-    db_user = 'lis_admin'
-    db_password = 'Bre91s_Admin'
+        db_name = 'SOME DB NAME'
+    db_user = 'SOME BD USER'
+    db_password = 'SOME PASSWORD'
     connect = pg.connect(f"host={host} port={port} dbname={db_name} user={db_user} password={db_password}")
 
